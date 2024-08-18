6c45a7803d883fece64599b6af57d5d22b35449d
Mustafa Dogan <mdogan89@hotmail.com>
2024-08-18 03:34:59 +0300

    connected form to api

--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useFormik } from "formik";
 import {
   Box,
@@ -15,28 +15,29 @@ import {
 import * as Yup from 'yup';
 import FullScreenSection from "./FullScreenSection";
 import useSubmit from "../hooks/useSubmit";
-import {useAlertContext} from "../context/alertContext";
+import { useAlertContext } from "../context/alertContext";
 
 const LandingSection = () => {
-  const {isLoading, response, submit} = useSubmit();
+  const { isLoading, response, submit } = useSubmit();
   const { onOpen } = useAlertContext();
 
   const formik = useFormik({
     initialValues: {
-      firstName: "",
-      email: "",
-      type: "hireMe",
-      comment: "",
+      name: "",
+      mail: "",
+      subject: "hireMe",
+      message: "",
     },
-    onSubmit: (values) => {submit("localhost:3000/#contact-me",values)},
+    onSubmit: (values) => { submit("https://littlelemonrestaurant-mdogan89s-projects.vercel.app/contact/", values) },
 
     validationSchema: Yup.object().shape({
-      firstName: Yup.string().required('Required'),
-      comment: Yup.string()
-      .min(25, 'Must be at least 25 characters')
-      .required('Required'), 
-      email: Yup.string().email('Invalid email address')
-      .required('Required')})
+      name: Yup.string().required('Required'),
+      message: Yup.string()
+        .min(25, 'Must be at least 25 characters')
+        .required('Required'),
+      mail: Yup.string().email('Invalid email address')
+        .required('Required')
+    })
   })
   useEffect(() => {
     if (response) {
@@ -61,28 +62,28 @@ const LandingSection = () => {
         <Box p={6} rounded="md" w="100%">
           <form onSubmit={formik.handleSubmit}>
             <VStack spacing={4}>
-              <FormControl isInvalid={formik.errors.firstName && formik.touched.firstName}>
-                <FormLabel htmlFor="firstName">Name</FormLabel>
+              <FormControl isInvalid={formik.errors.name && formik.touched.name}>
+                <FormLabel htmlFor="name">Name</FormLabel>
                 <Input
-                  id="firstName"
-                  name="firstName"
-                  {...formik.getFieldProps("firstName")}
+                  id="name"
+                  name="name"
+                  {...formik.getFieldProps("name")}
                 />
-                <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
+                <FormErrorMessage>{formik.errors.name}</FormErrorMessage>
               </FormControl>
-              <FormControl isInvalid={formik.errors.email && formik.touched.email}>
-                <FormLabel htmlFor="email">Email Address</FormLabel>
+              <FormControl isInvalid={formik.errors.mail && formik.touched.mail}>
+                <FormLabel htmlFor="mail">Email Address</FormLabel>
                 <Input
-                  id="email"
-                  name="email"
+                  id="mail"
+                  name="mail"
                   type="email"
-                  {...formik.getFieldProps("email")}
+                  {...formik.getFieldProps("mail")}
                 />
-                <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+                <FormErrorMessage>{formik.errors.mail}</FormErrorMessage>
               </FormControl>
               <FormControl>
-                <FormLabel htmlFor="type">Type of enquiry</FormLabel>
-                <Select id="type" name="type" {...formik.getFieldProps("type")}>
+                <FormLabel htmlFor="subject">Type of enquiry</FormLabel>
+                <Select id="subject" name="subject" {...formik.getFieldProps("subject")}>
                   <option value="hireMe">Freelance project proposal</option>
                   <option value="openSource">
                     Open source consultancy session
@@ -90,15 +91,15 @@ const LandingSection = () => {
                   <option value="other">Other</option>
                 </Select>
               </FormControl>
-              <FormControl isInvalid={formik.errors.comment && formik.touched.comment}>
-                <FormLabel htmlFor="comment">Your message</FormLabel>
+              <FormControl isInvalid={formik.errors.message && formik.touched.message}>
+                <FormLabel htmlFor="message">Your message</FormLabel>
                 <Textarea
-                  id="comment"
-                  name="comment"
+                  id="message"
+                  name="message"
                   height={250}
-                  {...formik.getFieldProps("comment")}
+                  {...formik.getFieldProps("message")}
                 />
-                <FormErrorMessage>{formik.errors.comment}</FormErrorMessage>
+                <FormErrorMessage>{formik.errors.message}</FormErrorMessage>
               </FormControl>
               <Button type="submit" colorScheme="purple" width="full" isLoading={isLoading}>
                 Submit
--- src/hooks/useSubmit.js
+++ src/hooks/useSubmit.js
@@ -1,5 +1,5 @@
-import {useState} from "react";
-
+import { useState } from "react";
+import axios from "axios";
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 /**
@@ -11,27 +11,23 @@ const useSubmit = () => {
   const [response, setResponse] = useState(null);
 
   const submit = async (url, data) => {
-    const random = Math.random();
     setLoading(true);
-    try {
-      await wait(2000);
-      if (random < 0.5) {
-        throw new Error("Something went wrong");
-      }
+    axios.post(url, data).then(response => {
+      console.log(response)
       setResponse({
         type: 'success',
-        message: `Thanks for your submission ${data.firstName}, we will get back to you shortly!`,
+        message: `Thanks for your submission ${data.name}, we will get back to you shortly!`,
       })
-    } catch (error) {
+    }).catch(error => {
+      console.log(error)
       setResponse({
         type: 'error',
         message: 'Something went wrong, please try again later!',
       })
-    } finally {
-      setLoading(false);
-    }
-  };
+    }).finally(setLoading(false))
 
+
+  }
   return { isLoading, response, submit };
 }
 
